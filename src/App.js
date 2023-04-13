4c676dfb98b1b15bcffc4f606a68a4c7f393404d
PolatB <burakpolat4141@gmail.com>
2023-04-13 16:56:05 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import PokemonList from "./Pages/PokemonList/PokemonList";
+import PokemonDetails from "./Pages/PokemonDetails/PokemonDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <nav className="routes">
+          <Link to="/">List</Link>
+          <Link to="/details">Details</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<PokemonList />} />
+          <Route path="/details" element={<PokemonDetails />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
